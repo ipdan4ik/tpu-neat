60f4cfce1ff105b237405654ac7ef882ebeaf31a
ipu <uwu@ipu.moe>
2024-06-10 18:10:35 +0300

    add innovation_tracker; add basic genome methods

--- main.py
+++ main.py
@@ -8,6 +8,18 @@ class NodeGene:
         self.layer = layer
 
 
+class ConnectionId(tuple):
+    def __new__(cls, from_id: int, to_id: int):
+        return super(ConnectionId, cls).__new__(cls, (from_id, to_id))
+
+    def __init__(self, from_id: int, to_id: int):
+        self.from_id = from_id
+        self.to_id = to_id
+
+    def __reduce__(self):
+        return self.__class__, (self.from_id, self.to_id)
+
+
 class ConnectionGene:
     def __init__(
         self,
@@ -25,11 +37,54 @@ class ConnectionGene:
 
     @property
     def connection_id(self):
-        return self.from_node.node_id, self.to_node.node_id
+        return ConnectionId(
+            from_id=self.from_node.node_id,
+            to_id=self.to_node.node_id,
+        )
+
+
+class InnovationTracker:
+    def __init__(self):
+        self.current_innovation_number = 0
+        self.innovations = {}
+
+    def get_innovation_number(self, in_node: NodeGene, out_node: NodeGene):
+        key = (in_node.node_id, out_node.node_id)
+        if key not in self.innovations:
+            self.innovations[key] = self.current_innovation_number
+            self.current_innovation_number += 1
+        return self.innovations[key]
+
+    def reset_innovations(self):
+        self.innovations = {}
+        self.current_innovation_number = 0
+
+
+innovation_tracker = InnovationTracker()
 
 
 class Genome:
     def __init__(self):
-        self.connections: dict[tuple[int, int], ConnectionGene] = dict()
+        self.connections: dict[ConnectionId, ConnectionGene] = dict()
         self.nodes: list[NodeGene] = list()
         self.fitness: float = 0
+
+    def add_node(self, layer: int) -> NodeGene:
+        node = NodeGene(node_id=len(self.nodes), layer=layer)
+        self.nodes.append(node)
+        return node
+
+    def add_connection(
+        self, in_node: NodeGene, out_node: NodeGene, weight: float
+    ) -> ConnectionGene:
+        connection = ConnectionGene(
+            from_node=in_node,
+            to_node=out_node,
+            weight=weight,
+            enabled=True,
+            innovation_number=innovation_tracker.get_innovation_number(
+                in_node, out_node
+            ),
+        )
+        self.connections[connection.connection_id] = connection
+        return connection
